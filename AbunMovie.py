af13c368c3b29fafc5dc07ec2321d163018dc1fb
OriolAbril <oriol.abril.pla@gmail.com>
2017-02-27 17:44:18 +0100

    Add files via upload

---
+++ AbunMovie.py
@@ -0,0 +1,40 @@
+'''
+Simple python script to generate a mp4 movie using stellar
+data stored in MESA .data files made available through 
+mesa class
+'''
+import os, sys
+import numpy as np
+
+#...Video & plot specifics
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import matplotlib.animation as manimation
+
+#...Mesa specifics
+import mesaPlot as mp
+import mesa_reader as ms
+
+m=mp.MESA() #initialize mesaPlot instance to read and plot data
+
+#...how to step through the .data sequence
+fold=ms.MesaLogDir() 
+models = fold.model_numbers #get all available models from profiles.index
+
+FFMpegWriter = manimation.writers['ffmpeg']
+metadata = dict(title='Isotopic abundances evolution in a ms 1M star', artist='Matplotlib')
+writer = FFMpegWriter(fps=5, metadata=metadata)
+
+fig=plt.figure(figsize=(12,12))
+ax=fig.add_axes([0.2,0.15,0.7,0.75])
+
+with writer.saving(fig,"AbunEvolMovie.mp4",len(models)):
+    for mod_no in models:
+        ax.cla()
+        m.loadProfile(num=mod_no)
+        p=mp.plot()
+        p.plotAbun(m,show=False,fig=fig,ax=ax)
+        ax.set_xlim(0,1)
+        writer.grab_frame()
+
---
+++ grafics_mesa.py
@@ -0,0 +1,104 @@
+import argparse as arp
+import numpy as np
+import mesa_reader as ms
+import re
+
+p=arp.ArgumentParser(prog='MESAplot',description='Script to plot data from the .data output files from MESA')
+p.add_argument('--version',action='version',version='%(prog)s 0.1')
+p.add_argument('files',metavar='FILES',help='Name of the .data type file/s with the extension',nargs='+')
+group=p.add_mutually_exclusive_group(required=True)
+group.add_argument('-hd','--headers',help='Show available headers in the files. Default: False',action='store_true',default=False)
+group.add_argument('-c','--columns',help='Columns to be plotted, the first one will be used as x values. The separation between column  mes for the same file should be a comma and between different files a space.',nargs='+')
+p.add_argument('-s','--separator',help='Separator character/s or regular expression to match the separator',type=str,default=r'\s+')
+pltpar=p.add_argument_group(title='Commands to personalize plots')
+pltpar.add_argument('-mp',help='Do not show plot with matplotlib. Default: True',action='store_false',default=True)
+pltpar.add_argument('-off','--offset',help='Use matplotlib''s default offest in y axis. Default: False',action='store_false',default=True)
+pltpar.add_argument('-eps',help='Write plot as an Encapsulated PostScript. The name can be specified',type=str,nargs='?',default='noeps',const='sieps')
+pltpar.add_argument('-sc','--scale',help='Choose the scale between: liner, semilogy, semilogx or logxy',type=str,choices=['lin','logy','logx','loglog','logxy'])
+pltpar.add_argument('-co','--colors',help='Colors to be used in the plot, they must be valid matplotlib colors',nargs='+')
+pltpar.add_argument('-l','--legend',help='Labels for the legend. Default text is name of file without extension',nargs='+')
+pltpar.add_argument('-t','--title',help='Title of the plot',type=str,default='')
+pltpar.add_argument('-lt','--legtit',help='Title for the legend',type=str,default='')
+pltpar.add_argument('-xl','--xlabel',help='Label of the x axis',type=str,default='')
+pltpar.add_argument('-yl','--ylabel',help='Label of the y axis',type=str,default='')
+pltpar.add_argument('-x','--xaxis',help='Set the xaxis limits',nargs=2,type=float)
+pltpar.add_argument('-y','--yaxis',help='Set the yaxis limits',nargs=2,type=float)
+args=p.parse_args() #parse arguments
+if not(args.headers):
+    if args.scale=='loglog':
+        args.scale='logxy'
+    l1=len(args.files)
+    colo=['blue','black','red','green','yellow','cyan','magenta']*3 #set default colors
+    colus=args.columns
+    if args.colors: #if flac -co present, overwrite default colors
+        for colnum,col in enumerate(args.colors):
+            colo[colnum]=col
+
+    if (args.mp or args.eps!='noeps'): #import and initialize plot
+        import matplotlib.font_manager as fnt
+        import matplotlib.pyplot as plt
+        if args.offset:
+            import matplotlib.ticker as tk
+            marques=tk.ScalarFormatter(useOffset=False)
+        xlab=args.xlabel
+        fig=plt.figure(1)
+        graf=fig.add_axes([0.13,0.1, 0.8, 0.8])
+
+colcount=0
+legcount=0 #legend label counter
+fpat=re.compile(r'(?P<nom>[^/\.]+)\.') #regular expression to obtain the name of the file without extension
+for filecount,doc in enumerate(args.files): #loop over each file
+    fd=open(doc,'r')
+    h=ms.MesaData(doc)
+    if args.headers:
+        print doc
+        print h.bulk_names
+    else:
+        docols=[col for col in re.split(',',colus[filecount])] 
+        numplots=len(docols)-1
+        if args.legend:
+            leg=args.legend[legcount:numplots]
+            legcount+=numplots
+        else:
+            leg=['']*numplots
+            for numpl,pl in enumerate(docols[1:]):
+                leg[numpl]=pl
+        x=h.data(docols[0])
+        if (args.mp or args.eps!='noeps'):
+            for i in range(numplots):
+                y=h.data(docols[i+1])
+                graf.plot(x,y,color=colo[colcount],label=leg[i])
+                colcount+=1
+if not(args.headers):
+    if (args.mp or args.eps!='noeps'):
+        graf.set_xlabel(args.xlabel)
+        graf.set_ylabel(args.ylabel)
+        if args.yaxis: 
+            graf.set_ylim(args.yaxis)
+        if args.xaxis:
+            graf.set_xlim(args.xaxis)
+        if args.offset:
+            graf.yaxis.set_major_formatter(marques)
+        if (args.scale=='logx' or args.scale=='logxy'):
+            graf.set_xscale('log')
+        if (args.scale=='logy' or args.scale=='logxy'):
+            graf.set_yscale('log')
+        fig.suptitle(args.title)
+        graf.grid(True)
+        graf.legend(loc='best', prop=fnt.FontProperties(size='medium'),title=args.legtit)
+    if args.eps!='noeps':
+        if args.eps!='sieps':
+            seed=fpat.search(args.eps)
+            if seed:
+                figname=seed.group('nom')+'.eps'
+            else:
+                figname=args.eps+'.eps'
+        else:
+            seed=fpat.search(args.files[0])
+            if seed:
+                figname=seed.group('nom')+'.eps'
+            else:
+                figname=args.files[0]+'.eps'
+        fig.savefig(figname, format='eps', dpi=1000)
+    if args.mp:    
+        plt.show()
