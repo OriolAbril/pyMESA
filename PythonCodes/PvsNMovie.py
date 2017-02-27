20d340eacbb01fa37563a8e2a7349ec391e4dcb8
Oriol Abril <oriol.abril.pla@gmail.com>
2017-02-27 19:59:24 +0100

    PvsNMovie.py added

---
+++ PythonCodes/PvsNMovie.py
@@ -0,0 +1,34 @@
+import os, sys
+import numpy as np
+
+#...Video & plot specifics
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+import matplotlib.animation as manimation
+
+#...Mesa specifics
+import mesaPlot as mp
+import mesa_reader as ms
+
+m=mp.MESA() #initialize mesaPlot instance to read and plot data
+
+#...how to step through the .data sequence
+fold=ms.MesaLogDir() 
+models = fold.model_numbers #get all available models from profiles.index
+
+FFMpegWriter = manimation.writers['ffmpeg']
+metadata = dict(title='Isotopic abundances evolution in a ms 1M star', artist='Matplotlib')
+writer = FFMpegWriter(fps=5, metadata=metadata)
+
+
+fig=plt.figure(figsize=(12,12))
+with writer.saving(fig,"PvsNAbunMovie.mp4",len(models)):
+    for mod_no in models:
+        ax=fig.add_axes([0.2,0.15,0.7,0.75])
+        m.loadProfile(num=mod_no)
+        p=mp.plot()
+        p.plotAbunPAndN(m,show=False,fig=fig,ax=ax)
+        ax.set_xlim(0,1)
+        writer.grab_frame()
+        fig.clf()
