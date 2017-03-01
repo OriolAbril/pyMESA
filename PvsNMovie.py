37f2bbb5237b64a8a68d4516cfb5c12ce20ef4a9
Oriol Abril: ixion <oriol.abril.pla@gmail.com>
2017-03-01 12:11:03 +0100

    PvsNMovie.py axis modified
    Axis aspect ratio set to equal and bottom limit for both x and y axis set to -0.5

--- PvsNMovie.py
+++ PvsNMovie.py
@@ -1,34 +1,42 @@
 import os, sys
 import numpy as np
 
-#...Video & plot specifics
+#Video & plot 
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.animation as manimation
 
-#...Mesa specifics
+#Mesa specifics
 import mesaPlot as mp
-import mesa_reader as ms
+#import mesa_reader as ms
 
 m=mp.MESA() #initialize mesaPlot instance to read and plot data
 
-#...how to step through the .data sequence
-fold=ms.MesaLogDir() 
-models = fold.model_numbers #get all available models from profiles.index
+#find all profiles to import with mesa_reader
+#fold=ms.MesaLogDir() 
+#models = fold.model_numbers #get all available models from profiles.index
+
+#find all profiles to import with mesaPlot
+m._loadProfileIndex('LOGS')
+models=m.prof_ind['model']
 
 FFMpegWriter = manimation.writers['ffmpeg']
 metadata = dict(title='Isotopic abundances evolution in a ms 1M star', artist='Matplotlib')
 writer = FFMpegWriter(fps=5, metadata=metadata)
 
 
-fig=plt.figure(figsize=(12,12))
+fig=plt.figure(figsize=(14,12))
 with writer.saving(fig,"PvsNAbunMovie.mp4",len(models)):
     for mod_no in models:
         ax=fig.add_axes([0.2,0.15,0.7,0.75])
         m.loadProfile(num=mod_no)
         p=mp.plot()
         p.plotAbunPAndN(m,show=False,fig=fig,ax=ax)
-        ax.set_xlim(0,1)
+        xb,xt=ax.get_xlim() #xbottom,xtop
+        yb,yt=ax.get_ylim() #ybottom,ytop
+        ax.set_xlim(-0.5,max(xt,yt))
+        ax.set_ylim(-0.5,max(xt,yt))
+        ax.set_aspect('equal')
         writer.grab_frame()
         fig.clf()
