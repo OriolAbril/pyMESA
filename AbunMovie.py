0cc3c3235d909aa3165695a0c5ae50740cdda0ed
Oriol Abril: ixion <oriol.abril.pla@gmail.com>
2017-03-02 12:25:48 +0100

    Command line parsing added to AbunMovie.py

--- AbunMovie.py
+++ AbunMovie.py
@@ -1,5 +1,5 @@
 import os, sys
-import numpy as np
+import argparse as arp
 
 #Video & plot specifics
 import matplotlib
@@ -9,26 +9,64 @@ import matplotlib.animation as manimation
 
 #Mesa specifics
 import mesaPlot as mp
-import mesa_reader as msr
+
+p=arp.ArgumentParser(prog='AbunVideo',description='Script to generate videos from MESA profile files. It uses mesaPlot')
+p.add_argument('--version',action='version',version='%(prog)s 0.0')
+p.add_argument('-t','--title',help='Title of the plot',default=None)
+p.add_argument('-age',help='Show star age in the title',action='store_true',default=False)
+p.add_argument('-xn','--xname',help='Name of the column to be used as x data',type=str,default='mass')
+p.add_argument('-xl','--xlabel',help='Label of the x axis',default=None)
+p.add_argument('-yl','--ylabel',help='Label of the y axis',default=None)
+p.add_argument('-xlim',help='Set the xaxis limits',nargs=2,type=float)
+args=p.parse_args() #parse arguments
+
+if args.xlim:
+    xmin=args.xlim[0]
+    xmax=args.xlim[1]
+else:
+    xmin=None
+    xmax=None
 
 m=mp.MESA() #initialize mesaPlot instance to read and plot data
 
-#how to step through the .data sequence
-fold=msr.MesaLogDir() 
-models = fold.model_numbers #get all available models from profiles.index
+#find all profiles to import with mesaPlot
+m._loadProfileIndex('LOGS')
+models=m.prof_ind['model']
+
+try:
+    m.loadProfile(num=models[0],silent=True)
+    x=m.prof.data[args.xname]
+    if not(args.xlim):
+        xmin=min(x)
+        xmax=max(x)
+except (KeyError,AttributeError):
+    raise ValueError(args.xname+"not found as data name")
+
+fig=plt.figure(figsize=(12,12))
+ax=fig.add_axes([0.2,0.15,0.7,0.75])
+p=mp.plot()
+try:
+    p.plotAbun(m,show=False,fig=fig,ax=ax,xaxis=args.xname,xmin=xmin,xmax=xmax,show_title_age=args.age,ylabel=args.ylabel,title=args.title,xlabel=args.xlabel)
+except ValueError:
+    aux=xmax
+    xmax=xmin
+    xmin=aux
+    p.plotAbun(m,show=False,fig=fig,ax=ax,xaxis=args.xname,xmin=xmin,xmax=xmax,show_title_age=args.age,ylabel=args.ylabel,title=args.title,xlabel=args.xlabel)
+
+xaxlim=ax.get_xlim()
+yaxlim=ax.get_ylim()
+axratio=ax.get_aspect()
 
 FFMpegWriter = manimation.writers['ffmpeg']
 metadata = dict(title='Isotopic abundances evolution in a ms 1M star', artist='Matplotlib')
 writer = FFMpegWriter(fps=5, metadata=metadata)
-
-fig=plt.figure(figsize=(12,12))
-ax=fig.add_axes([0.2,0.15,0.7,0.75])
-
 with writer.saving(fig,"AbunEvolMovie.mp4",len(models)):
     for mod_no in models:
         ax.cla()
         m.loadProfile(num=mod_no)
         p=mp.plot()
-        p.plotAbun(m,show=False,fig=fig,ax=ax)
-        ax.set_xlim(0,1)
+        p.plotAbun(m,show=False,fig=fig,ax=ax,xaxis=args.xname,xmin=xmin,xmax=xmax,show_title_age=args.age,ylabel=args.ylabel,title=args.title,xlabel=args.xlabel)
+        ax.set_xlim(xaxlim)
+        ax.set_ylim(yaxlim)
+        ax.set_aspect(axratio)
         writer.grab_frame()
